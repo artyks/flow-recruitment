c2165803e158f8a32c35aaf405f76c4eae5f9d35
Artem Karatai <artyom.karatai@gmail.com>
2023-06-23 20:45:04 +0200

    [app][api][product-categories] complete service

--- apps/api/src/product-categories/services/product-categories/product-categories.service.ts
+++ apps/api/src/product-categories/services/product-categories/product-categories.service.ts
@@ -1,4 +1,30 @@
+import { PrismaClientService } from '@flow-recruitment/prisma';
+import { CreateProductCategoryDto } from '@flow-recruitment/product-categories/dtos';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ProductCategoriesService {}
+export class ProductCategoriesService {
+  private readonly repository: PrismaClientService['productCategory'];
+
+  constructor(prisma: PrismaClientService) {
+    this.repository = prisma.productCategory;
+  }
+
+  async findOne(id: string) {
+    return await this.repository.findUnique({ where: { id } });
+  }
+
+  async findMany() {
+    return await this.repository.findMany();
+  }
+
+  async createOne(payload: CreateProductCategoryDto) {
+    return await this.repository.create({
+      data: {
+        name: payload.name,
+        formPurchase: { connect: { id: payload.formPurchaseId } },
+        formSearch: { connect: { id: payload.formSearchId } },
+      },
+    });
+  }
+}
