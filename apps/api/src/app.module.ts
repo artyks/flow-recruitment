1278b79e0d40ca7dcca30f5d5418397fb0b4d669
Artem Karatai <artyom.karatai@gmail.com>
2023-06-23 20:26:18 +0200

    [app][api] import Prisma module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,9 +5,11 @@ import { loadConfig } from './config/config';
 import { FormsModule } from './forms/forms.module';
 import { ProductCategoriesModule } from './product-categories/product-categories.module';
 import { UsersModule } from './users/users.module';
+import { PrismaModule } from '@flow-recruitment/prisma';
 
 @Module({
   imports: [
+    PrismaModule,
     ConfigModule.forRoot({
       isGlobal: true,
       load: [loadConfig],
